666299148e69f5f4c1704d4bf151b36cecaa326b
mhe-ce <mike.he@communityengine.com>
2012-09-24 13:56:16 +1000

    homework week 2, draft

---
+++ homework/week 2/script.R
@@ -0,0 +1,23 @@
+# statistics one, homework, week two
+# read spatial working memory, descriptive statistics, 8x8 matrix correlation
+
+# load psych library to use descriptive statistics function
+library(psych)
+
+# load data and show the fields
+data <- read.table("DAA.02.txt", header=T)
+names(data)
+
+describe(data$pre.wm.s1[data$cond=="des"])
+describe(data$pre.wm.s1[data$cond=="aer"])
+describe(data$post.wm.s1[data$cond=="des"])
+describe(data$post.wm.s1[data$cond=="aer"])
+describe(data$pre.wm.v1[data$cond=="des"])
+describe(data$pre.wm.v1[data$cond=="aer"])
+describe(data$post.wm.s1[data$cond=="des"])
+describe(data$post.wm.s1[data$cond=="aer"])
+
+plot(data$pre.wm.s1, data$post.wm.s1)
+abline(lm(data$post.wm.s1 ~ data$pre.wm.s1))
+plot(data$pre.wm.s2, data$post.wm.s2)
+abline(lm(data$post.wm.s2 ~ data$pre.wm.s2))
