cefb3631e5fbb0ac8d9325be450dbe4bc385459c
misaxi <misaxi@gmail.com>
2012-10-02 23:32:52 +1000

    refactoring for week 1 assignment

--- homework/week 1/script.r
+++ homework/week 1/script.r
@@ -5,20 +5,19 @@
 library(psych)
 
 # read data into dataframe
-aer = read.table("DAA.01.aer.txt", header=TRUE)
-des = read.table("DAA.01.des.txt", header=TRUE)
+exp = read.table("DAA.01.txt", header=TRUE)
 
 # plot 8 histograms into one layout with 2 rows and 4 columns
 layout(matrix(c(1,2,3,4,5,6,7,8),2,4,byrow=TRUE))
-hist(des$pre.wm.s, xlab="pre.wm.s")
-hist(des$post.wm.s, xlab="post.wm.s")
-hist(des$pre.wm.v, xlab="pre.wm.v")
-hist(des$post.wm.v, xlab="post.wm.v")
-hist(aer$pre.wm.s, xlab="pre.wm.s")
-hist(aer$post.wm.s, xlab="post.wm.s")
-hist(aer$pre.wm.v, xlab="pre.wm.v")
-hist(aer$post.wm.v, xlab="post.wm.v")
+
+hist(exp$pre.wm.s[exp$cond=="aer"], xlab="pre.wm.s")
+hist(exp$post.wm.s[exp$cond=="aer"], xlab="post.wm.s")
+hist(exp$pre.wm.v[exp$cond=="aer"], xlab="pre.wm.v")
+hist(exp$post.wm.v[exp$cond=="aer"], xlab="post.wm.v")
+hist(exp$pre.wm.s[exp$cond=="des"], xlab="pre.wm.s")
+hist(exp$post.wm.s[exp$cond=="des"], xlab="post.wm.s")
+hist(exp$pre.wm.v[exp$cond=="des"], xlab="pre.wm.v")
+hist(exp$post.wm.v[exp$cond=="des"], xlab="post.wm.v")
 
 # describes data
-describe(des)
-describe(aer)
+describeBy(exp, exp$cond)
